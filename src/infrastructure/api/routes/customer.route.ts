f6127ed2ae8032b070925660afd183bfb7cb317c
Mateus Picoloto <mateus.picoloto97@gmail.com>
2024-09-23 08:35:16 -0300

    feat: added customer list and create routes

---
+++ src/infrastructure/api/routes/customer.route.ts
@@ -0,0 +1,37 @@
+import express, {Request, Response} from 'express';
+import CreateCustomerUsecase from '../../../usecase/customer/create/create.customer.usecase';
+import CustomerRepository from '../../customer/repository/sequelize/customer.repository';
+import ListCustomerUseCase from '../../../usecase/customer/list/list.customer.usecase';
+
+export const customerRoute = express.Router();
+
+customerRoute.post('/', async (req: Request, res: Response) => {
+    const useCase = new CreateCustomerUsecase(new CustomerRepository());
+
+    try {
+        const customerDto = {
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                city: req.body.address.city,
+                number: req.body.address.number,
+                zip: req.body.address.zip,
+            }
+        }
+
+        const output = await useCase.execute(customerDto);
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+    const useCase = new ListCustomerUseCase(new CustomerRepository());
+    try {
+        const output = await useCase.execute({});
+        res.send(output)
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
