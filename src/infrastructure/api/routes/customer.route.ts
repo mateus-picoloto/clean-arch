1b41b1f9884abf87665cdb9ead5b420663a677bd
Mateus Picoloto <mateus.picoloto97@gmail.com>
2024-09-27 19:49:13 -0300

    feat: added customer presenter

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from 'express';
 import CreateCustomerUsecase from '../../../usecase/customer/create/create.customer.usecase';
 import CustomerRepository from '../../customer/repository/sequelize/customer.repository';
 import ListCustomerUseCase from '../../../usecase/customer/list/list.customer.usecase';
+import { json } from 'sequelize';
+import CustomerPresenter from '../presenters/customer.presenter';
 
 export const customerRoute = express.Router();
 
@@ -28,10 +30,10 @@ customerRoute.post('/', async (req: Request, res: Response) => {
 
 customerRoute.get("/", async (req: Request, res: Response) => {
     const useCase = new ListCustomerUseCase(new CustomerRepository());
-    try {
-        const output = await useCase.execute({});
-        res.send(output)
-    } catch (err) {
-        res.status(500).send(err);
-    }
+    const output = await useCase.execute({});
+
+    res.format({
+        json: async () => res.send(output),
+        xml: async () => res.send(CustomerPresenter.listXML(output)),
+    })
 });
